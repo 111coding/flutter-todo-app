5e95484148ae985903e480e5db48f10d229697f3
111coding <jwlee1024@gmail.com>
2025-08-12 21:01:50 +0900

    feat: 버튼 스타일

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -84,7 +84,6 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 11),
-                      // TODO 저장 버튼 꾸미기!
                       TextField(
                         decoration: InputDecoration(
                           border: OutlineInputBorder(
@@ -101,7 +100,31 @@ class HomePage extends StatelessWidget {
                         ),
                       ),
                       Spacer(),
-                      ElevatedButton(onPressed: () {}, child: Text("저장")),
+                      // ElevatedButton 속성에는 크기를 정할 수 있는 속성이 없음
+                      // 부모위젯의 크기가 있다면 => 부모위젯의 크기만큼 버튼크기가 확장됨
+                      // 없다면 => 자녀 위젯의 크기만큼 축소됨
+                      // double.infinity => 소수 표현하는 타입 double.
+                      // => double(소수) 이 가질 수 있는 값중에서 가장 큰값(무한)
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          // ButtonStyle 클래스 사용하면
+                          // WidgetStatePropertyAll(Colors.red)
+                          // 이런식으로 객체 하나 더 감싸줘야돼서
+                          // 코드가 길어짐
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF5714E6),
+                            foregroundColor: Colors.white, // 버튼 내 아이콘, 텍스트 모두 적용됨
+                            textStyle: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Text("저장"),
+                        ),
+                      ),
                     ],
                   ),
                 ),
